d4a944c2eb47e883d511ab43b39dec1b6ea915f5
Tobias Roland <tobiasroland@gmail.com>
2019-07-04 12:00:15 +0100

    Blank project

---
+++ src/test/java/I_AM_TEST.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Run this and make it go green, then delete this class before starting your work
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class I_AM_TEST {
+
+    @Test
+    public void shouldTestThing() {
+        assertThat(true).isTrue();
+    }
+}
